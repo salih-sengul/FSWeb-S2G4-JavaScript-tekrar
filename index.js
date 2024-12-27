6b5c7780c38cac53bdc745ad57f5bed8d565899d
salih-sengul <salih.sengul@outlook.com>
2024-12-27 20:51:42 +0300

    CemberCevre

--- index.js
+++ index.js
@@ -50,8 +50,8 @@ concole.log(KareninAlani(10));
 	4. Hesaplanan çemberin çevresi döndürülecektir.
 */
 
-function CemberinCevresi(/* kodlar buraya */) {
-  /* kodlar buraya */
+function CemberinCevresi(r) {
+  return 2 * pi * r;
 }
 
 /* (Oto test yok) Yukarıdaki CemberinCevresi fonksiyonunu yarıçap = 5 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
